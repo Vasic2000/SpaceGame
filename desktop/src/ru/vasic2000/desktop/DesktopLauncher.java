220376d2071916211e1f6407b71a2b9d6d46e249
Vasic2000 <Vasic2000@yandex.ru>
2019-05-17 11:13:18 +0300

    StartCommit

---
+++ desktop/src/ru/vasic2000/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.vasic2000.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.vasic2000.SpaceGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceGame(), config);
+	}
+}
